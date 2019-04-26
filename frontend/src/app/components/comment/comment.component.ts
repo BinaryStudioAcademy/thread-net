7338bbc78b64499fbc93ae0eabfe40e8d7b800cc
TFS\Oleg <oleg.kopot@binary-studio.com>
2019-04-26 18:14:53 +0300

    Changed format of date

--- frontend/src/app/components/comment/comment.component.ts
+++ frontend/src/app/components/comment/comment.component.ts
@@ -12,23 +12,4 @@ export class CommentComponent implements OnInit {
     public constructor() {}
 
     public ngOnInit() {}
-
-    public getCommentDate(date: Date) {
-        const createdAt = new Date(date);
-        const currentDate = new Date();
-        const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-
-        const days = currentDate.getDate() - createdAt.getDate();
-        const hours = currentDate.getHours() - createdAt.getHours();
-        const minutes = currentDate.getMinutes() - createdAt.getMinutes();
-
-        if (currentDate > createdAt && days === 0 && hours < 24) {
-            if (minutes < 60) {
-                return `${minutes} minutes ago`;
-            }
-            return `${hours} hours ago`;
-        }
-
-        return `${createdAt.getDate()} ${monthNames[createdAt.getMonth()]}`;
-    }
 }
