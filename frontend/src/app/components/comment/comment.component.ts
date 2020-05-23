327299c2187764ec16689470027dc12c1eeeac91
SergeySerg123 <sergey_chizhik@ukr.net>
2020-05-24 02:12:55 +0300

    Update comment.component.ts

--- frontend/src/app/components/comment/comment.component.ts
+++ frontend/src/app/components/comment/comment.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Comment } from '../../models/comment/comment';
+import { User } from 'src/app/models/user';
 
 @Component({
     selector: 'app-comment',
@@ -8,4 +9,16 @@ import { Comment } from '../../models/comment/comment';
 })
 export class CommentComponent {
     @Input() public comment: Comment;
+    @Input() public currentUser: User;
+    @Output() public deleteClick = new EventEmitter<number>();
+
+    public deleteComment(commentId: number) {
+        this.deleteClick.emit(commentId);
+    }
+
+    public isAuthor() {
+        let currentUserId = this.currentUser.id;
+        let commentUserId = this.comment.author.id;
+        return currentUserId === commentUserId;
+    }
 }
